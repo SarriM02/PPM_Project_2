fe2a4ebc7f6ff879f9572167322199fa66ac9f6e
Sarri02 <163002234+SarriM02@users.noreply.github.com>
2024-06-10 17:13:24 +0200

    Fix vercel.json

--- PPM_Project/urls.py
+++ PPM_Project/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from PPM_App.Views import PollCreateView, Register, Dashboard, Logout, PollCreate, Delete_Poll, vote
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='Login.html'), name='Login'),
-    path('polls/', PollCreateView.as_view(), name='create_poll'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('register/', Register, name='Register'),
